39610d66bd3cb2c6c2cae40d64c6f9a57d67afaa
Apurbasi011 <92560286+Apurbasi011@users.noreply.github.com>
2021-10-31 06:46:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,114 @@
+prediction = "";
+
+
+Webcam.set
+({
+
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality : 90
+
+});
+
+  camera = document.getElementById("camera");
+
+  Webcam.attach('#camera');
+
+  function take_snapshot()
+  {
+
+    Webcam.snap(function(data_uri)
+    {
+
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'
+
+    });
+
+  }
+
+  console.log('ml5 version' , ml5.version);
+  classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/VhnnhMdyN/model.json', modelLoaded);
+
+  function modelLoaded()
+  {
+
+    console.log('Model Ready!');
+
+  }
+
+  function speak_data()
+  {
+
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The First Prediction Is " +prediction_1;
+    speak_data_2 = "The Second Prediction Is " +prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+
+  }
+
+  function check()
+  {
+
+    img = document.getElementById('captured_image');
+    classifier.classify(img , gotResult);
+
+  }
+
+  function gotResult(error , results) 
+  {
+
+    if (error)
+    {
+
+      console.log(error);
+
+    }
+
+    else 
+    {
+
+      console.log(results);
+      document.getElementById("result_emotion_name").innerHTML = results[0].label;
+      prediction = results[0].label;
+      speak();
+
+     if(results[0].label == "Best")
+    {
+
+      document.getElementById("update_emoji").innerHTML = "&#128077; All The Best";
+
+    }
+
+    if(results[0].label == "Victory")
+    {
+
+      document.getElementById("update_emoji").innerHTML = "&#9996; That Was a Marvelous Victory";
+
+    }
+
+    if(results[0].label == "Amazing")
+    {
+
+      document.getElementById("update_emoji").innerHTML = "&#128076; This Is Amazing";
+
+    }
+
+    if(results[0].label == "Namaste")
+    {
+
+      document.getElementById("update_emoji").innerHTML = "&#128591; Namaste";
+
+    }
+
+    if(results[0].label == "Stop")
+    {
+
+      document.getElementById("update_emoji").innerHTML = "&#9995; Please Stop";
+
+    }
+
+    }
+
+  }
